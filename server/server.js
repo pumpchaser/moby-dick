a87595c21ed9f34a7e7f7321ce83e16f12a68135
Fabrice Cheng <fabrice@springlabs.com>
2020-08-16 17:43:44 -0700

    add postgres integration

--- server/server.js
+++ server/server.js
@@ -5,6 +5,17 @@ const app = express();
 console.log("SERVER STARTING")
 app.use(express.static(path.join(__dirname, 'build')));
 
+// db Connection w/ localhost
+var db = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'moby_dick'
+  }
+});
+
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
