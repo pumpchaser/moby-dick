b9b6f5320fe752b1ca8f0fc5fdcc3f91f2356bd8
huy@gohuygo.com <huy@gohuygo.com>
2020-08-16 17:52:17 -0700

    Dashboard

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
 
 
 function App() {
   return (
       <Router>
         <Layout>
-          <Route exact path="/" />
+          <Route exact path="/" render={ () => <Dashboard />}/>
         </Layout>
       </Router>
   );
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react';
+import styled from 'styled-components'
+
+class Dashboard extends Component {
+  componentDidMount() {
+  }
+
+  render() {
+    return(
+      <Fragment>
+          <div className="center">
+            <h2>WhaleHunter</h2>
+          </div>
+      </Fragment>
+    )
+  }
+}
+
+export default Dashboard;
