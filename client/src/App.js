501459603142da330c7fd46ac5214d7f8e891f38
huy@gohuygo.com <huy@gohuygo.com>
2020-08-22 15:04:13 -0700

    remove warning

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
