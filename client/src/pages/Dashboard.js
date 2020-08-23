2a7307a6a0a6ff453d94c61923fd5b15dde1f67e
huy@gohuygo.com <huy@gohuygo.com>
2020-08-22 17:52:15 -0700

    Process myx contract

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,62 +2,32 @@ import React, { Component, Fragment } from 'react';
 import AddTokenModal from '../dashboard/AddToken';
 import { Button  } from 'semantic-ui-react'
 import web3 from '../web3';
+import axios from 'axios';
 
 class Dashboard extends Component {
   componentDidMount() {
   }
 
-  // async oldprocessTransactions() {
-    // console.log(process.env.REACT_APP_ETHERSCAN_API)
-    // console.log(process.env.REACT_APP_WEBSITE_NAME)
-    // const latestBlockNumber = await web3.eth.getBlockNumber();
-    // // console.log(latestBlockNumber)
-
-    // const block = await web3.eth.getBlock(latestBlockNumber)
-    // // console.log(block)
-
-    // const transactionHashes = block.transactions
-    // // console.log(transactionHashes)
-
-    // let transactions = [] 
-
-    // for (const txHash of transactionHashes) {
-    //   let tx = await web3.eth.getTransaction(txHash)
-    //   transactions.push(tx)
-    //   console.log(tx);
-
-    // }
-
-    // console.log(transactions)
-
-  // }
+  async getContractAbi(contractAddress) {
+    const etherscanURL = `https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`
+    const response = await axios.get(etherscanURL)
+    return JSON.parse(response.data.result)
+  }
 
   async processTransactions() {
     const contractAddress = '0x2129fF6000b95A973236020BCd2b2006B0D8E019'
-    const etherscanURL = `https://api.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`
-  
-    console.log(etherscanURL)
+    const contractAbi = await this.getContractAbi(contractAddress)
+
+    const contract = new web3.eth.Contract(contractAbi, contractAddress)
+    contract.events.Transfer(console.log)
   }
-// def init_contract(w3, address):
-//     url = f"https://api.etherscan.io/api?module=contract&action=getabi&address={address}&apikey={API_KEY}"
-//     resp = requests.get(url)
-//     json_resp = json.loads(resp.text)
-//     contract_abi = json_resp['result']
-//     my_contract = w3.eth.contract(address=address, abi=contract_abi)
-//     return my_contract
-
-
-// var myContract = new web3.eth.Contract([...], '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe', {
-//     from: '0x1234567890123456789012345678901234567891', // default from address
-//     gasPrice: '20000000000' // default gas price in wei, 20 gwei in this case
-// });
 
   render() {
     return(
       <Fragment>
           <div className="center">
             <AddTokenModal>Add Token</AddTokenModal>
-            <Button onClick={this.processTransactions}>Process Transactions</Button>
+            <Button onClick={this.processTransactions.bind(this)}>Process Transactions</Button>
 
           </div> 
       </Fragment>
