7b562652de9cc49138195ad2d621d11ddd65facf
huy@gohuygo.com <huy@gohuygo.com>
2020-08-30 22:33:54 -0700

    fix

--- src/reducers/reducer_events.js
+++ src/reducers/reducer_events.js
@@ -15,7 +15,7 @@ export function eventsReducer(state = [], action) {
       event['fromAddress'] = fromAddress
       event['transactionType'] = transactionType
     	existingEvents.unshift(event)
-      existingEvents.sort( (a, b) => b.timestamp - a.timestamp)
+      existingEvents.sort( (a, b) => b.blockNumber - a.blockNumber)
       return Object.assign([], existingEvents)
     default:
       return state
